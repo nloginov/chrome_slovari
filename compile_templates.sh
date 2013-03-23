ff722bcc2bbf5051ba10a18203a29cdd5d811475
nick <kolyanlab@yandex.ru>
2013-03-23 19:38:39 +0600

    JSLint satisfied

--- compile_templates.sh
+++ compile_templates.sh
@@ -1,16 +1,20 @@
 #!/usr/local/bin/node
-var hogan = require(__dirname + "/external/hogan.js")
+"use strict";
+/*jslint vars:true, white:true, plusplus:true, nomen:true */
+/*global  require*/
+
+var hogan = require(__dirname + "/external/hogan.js");
 var fs = require("fs");
 
 var filename = __dirname + "/templates/popover.hogan";
-var result = "var Templates = {};"
+var result = "var Templates = {};";
 
 fs.readFile(filename, function (err, content) {
     if(err) {
         throw err;
     }
-    var template = hogan.compile(content.toString(), {asString: true})
+    var template = hogan.compile(content.toString(), {asString: true});
     result += ";Templates.Popover = new Hogan.Template(" + template + ")";
 
-    fs.writeFile(__dirname + '/compiled_templates.js', result)
+    fs.writeFile(__dirname + '/compiled_templates.js', result);
 });
